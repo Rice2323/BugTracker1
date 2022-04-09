decf95277709e0930edd33c39ff9e9ea73a8eb8b
Brian Rice <brianrice2382@gmail.com>
2022-04-09 00:45:41 -0500

    update once more close fixed

--- frontend/src/Views/Components/bugCard/BugView/bugview.jsx
+++ frontend/src/Views/Components/bugCard/BugView/bugview.jsx
@@ -1,15 +1,19 @@
 import React  from "react";
 import  ViewSection  from "./component/bugviewsection";
 import  BugModel  from "../../../../Models/bugModel";
+import { useDispatch } from "react-redux";
+import { markCompleted } from "../../../../Controllers/Redux/bugSlice";
+
 import "./bugview.css"
 
 
 export default (props)=> {
     const bug = new BugModel(props.bug);
+    const dispatch = useDispatch();
 
     return (
         <div className="bug-view">
-            <button onClick={props.click} className='close-btn'>CLOSE</button>
+            <button onClick={props.clicked} className='close-btn'>CLOSE</button>
             <h1>{props.bug.name}</h1>
             <ViewSection title='Details' info = {bug.details} />
             <ViewSection title='Steps' info = {bug.steps} />
@@ -17,7 +21,7 @@ export default (props)=> {
             <ViewSection title='Creator' info = {bug.creator} />
             <ViewSection title='App Version' info = {bug.version} />
             <ViewSection title='Time Created' info = {bug.time} />
-            <button>Mark Complete</button>
+            <button onClick= {() => {dispatch(markCompleted)}}>Mark Complete</button>
         </div>
     )
 }
--- frontend/src/Views/Pages/viewBugs.jsx
+++ frontend/src/Views/Pages/viewBugs.jsx
@@ -30,7 +30,7 @@ export default () => {
             {bugs.map((bug,key)=>(
                 < BugCard key ={key} bug={bug} clicked={BugsClicked}/>
             ))}
-            {Display_Bug.isDisplayed && <BugView bug={bugs.filter((bug)=> bug.name == Display_Bug.name)[0]}/>}
+            {Display_Bug.isDisplayed && <BugView clicked={BugsClicked} bug={bugs.filter((bug)=> bug.name == Display_Bug.name)[0]}/>}
 
         </div>
     )
